02e274dcba2cff64aa18e39fe9a87604910c8bb1
Philip Schatz <phil@cnx.org>
2013-06-22 16:03:43 -0400

    If requirejs errors update the loading text and bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,24 @@
-require({
-  paths: {
-    cs: 'libs/require/plugins/require-cs/cs',
-    'coffee-script': 'libs/require/plugins/require-cs/coffee-script'
-  }
-}, ['cs!config']);
+(function() {
+
+  require({
+    paths: {
+      cs: 'libs/require/plugins/require-cs/cs',
+      'coffee-script': 'libs/require/plugins/require-cs/coffee-script'
+    }
+  }, ['cs!config']);
+
+  /* If an error occurs in requirejs then change the loading HTML. */
+  require.onError = function(err) {
+    "use strict";
+    var title = document.getElementById('loading-text'),
+        progress = document.getElementById('loading-progress');
+
+    if (title != null) {
+      title.textContent = 'Loading failed. Please try again in a bit.';
+    }
+    if (progress != null) {
+      progress.className = 'progress progress-danger';
+    }
+  };
+
+}) ();
